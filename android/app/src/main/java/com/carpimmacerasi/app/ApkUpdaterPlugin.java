fb81c918af6c306d37c654d704926c62693f5b9d
scbogretmen <scbogretmen@users.noreply.github.com>
2026-08-23 00:25:20 +0300

    Fix Capacitor plugin annotation import

--- android/app/src/main/java/com/carpimmacerasi/app/ApkUpdaterPlugin.java
+++ android/app/src/main/java/com/carpimmacerasi/app/ApkUpdaterPlugin.java
@@ -15,8 +15,8 @@ import androidx.core.content.FileProvider;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
-import com.getcapacitor.annotation.PluginMethod;
 
 import java.io.File;
 
